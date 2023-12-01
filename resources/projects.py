ac22edf2dfa3b8f99652cc7dcac25301032e9b54
Karolina Kupishevska <karolina_k@Karolinas-MacBook-Pro.local>
2023-12-01 14:04:37 +0200

    added basic routes for /users and /projects

---
+++ resources/projects.py
@@ -0,0 +1,73 @@
+import logging
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import SQLAlchemyError
+
+from schemas import ProjectSchema, UpdateProjectSchema
+from models.project import ProjectModel
+from db import db
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+blp = Blueprint('projects', __name__, description='Operations on projects')
+
+@blp.route('/projects/<string:project_id>')
+class projectProject(MethodView):
+
+  # GET PROJECT
+  @blp.response(200, ProjectSchema)
+  def get(self, project_id):
+    project = ProjectModel.query.get_or_404(project_id)
+    return project
+
+  # DELETE PROJECT METHOD
+  def delete(self, project_id):
+    # jwt = get_jwt()
+    # if not jwt.get('is_admin'):
+    #   abort(401, message='Blah-blah, no roots')
+    project = ProjectModel.query.get_or_404(project_id)
+    db.session.delete(project)
+    db.session.commit()
+    
+    return {'message': 'Project deleted'}, 200
+  
+  @blp.arguments(UpdateProjectSchema)
+  @blp.response(200, ProjectSchema)
+  def put(self, project_data, project_id):
+    logger.debug(f"Received project_data in put method: {project_data} {type(project_data)}")
+    if (project := ProjectModel.query.get_or_404(project_id)):
+      project.update_attributes(**project_data)
+    else: project = ProjectModel(id=project_id, **project_data)
+
+    logger.debug(f"{project} {type(project)}")
+
+    try:
+      db.session.add(project)
+      db.session.commit()
+    except SQLAlchemyError:
+      abort(500, message="Error occurred")
+
+    return project
+
+@blp.route('/projects')
+class projectList(MethodView):
+
+  # GET PROJECTS LIST
+  @blp.response(200, ProjectSchema(many=True))
+  def get(self):
+    return ProjectModel.query.all()
+  
+  # CREATE PROJECT METHOD
+  @blp.arguments(ProjectSchema)
+  @blp.response(201, ProjectSchema)
+  def post(self, project_data):
+    project = ProjectModel(**project_data)
+
+    try:
+      db.session.add(project)
+      db.session.commit()
+    except SQLAlchemyError:
+      abort(500, message="Error occurred")
+
+    return project
---
+++ resources/users.py
@@ -0,0 +1,93 @@
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from sqlalchemy.exc import SQLAlchemyError
+from passlib.hash import pbkdf2_sha256
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+
+from db import db
+
+from models import UserModel
+from schemas import UpdateUserSchema, UserSchema, LoginSchema, PlainUserSchema
+
+blp = Blueprint('Users', __name__, description='Operations with users')
+
+@blp.route('/register')
+class UserRegister(MethodView):
+  @blp.arguments(PlainUserSchema)
+  def post(self, user_data):
+    if UserModel.query.filter(UserModel.email == user_data['email']).first():
+      abort(409, message='A user with that email already exists.')
+
+    user = UserModel(**user_data)
+    user.password = pbkdf2_sha256.hash(user_data['password'])
+
+    try:
+      db.session.add(user)
+      db.session.commit()
+    except SQLAlchemyError as e:
+      abort(500, message=str(e))
+
+    return {'message': 'User successfuly created.'}, 201
+  
+@blp.route('/login')
+class UserLogin(MethodView):
+  @blp.arguments(LoginSchema)
+  def post(self, login_data):
+    user = UserModel.query.filter(UserModel.email == login_data['email']).first()
+    if user and pbkdf2_sha256.verify(login_data['password'], user.password):
+      identity = {
+        'id': user.id, 
+        'email': user.email, 
+        # 'role': user.role if user.role else 'user'
+        }
+      access_token = create_access_token(identity=identity)
+      return {'token': access_token}
+
+    abort(404, message='A user with such email and password does not exist.')
+
+# @blp.route('/logout')
+# class UserLogout(MethodView):
+#   @jwt_required()
+#   def post(self):
+#     jti = get_jwt()['jti']
+#     BLOCKLIST.add(jti)
+#     return {'message': 'Successfuly logged out.'}, 200
+
+
+@blp.route('/users')
+class UsersList(MethodView):
+  @blp.response(200, UserSchema(many=True))
+  def get(self):
+    return UserModel.query.all()
+  
+
+@blp.route('/users/<int:user_id>')
+class UserItem(MethodView):
+
+  @blp.response(200, UserSchema)
+  def get(self, user_id):
+    return UserModel.query.get_or_404(user_id)
+  
+  @blp.arguments(UpdateUserSchema)
+  @blp.response(200, UserSchema)
+  def put(self, user_data, user_id):
+    if user := UserModel.query.get_or_404(user_id):
+      user.update_attributes(**user_data)
+    else: user = UserModel(id=user_id, **user_data)
+
+    try:
+      db.session.add(user)
+      db.session.commit()
+    except SQLAlchemyError as e:
+      abort(500, message=str(e))
+
+    return user
+  
+  def delete(self, user_id):
+
+    user = UserModel.query.get_or_404(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return {'message': 'User is successfuly deleted'}, 200
